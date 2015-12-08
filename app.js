71239b7aef22420dd851477319782d31b449f820
larissaleite <larissa.leite@gmail.com>
2015-12-08 03:17:04 +0100

    Improved chat

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var app = require('http').createServer(handler);
-app.listen(8088);
 var io = require('socket.io').listen(app);
 var redis = require('redis');
 var fs = require('fs');
+
+app.listen(8088);
  
-function handler(req,res){
+function handler(req, res){
     fs.readFile(__dirname + '/index.html', function(err,data){
         if(err){
             res.writeHead(500);
@@ -16,29 +17,43 @@ function handler(req,res){
     });
 }
  
-var store = redis.createClient();
+//var store = redis.createClient();
 var pub = redis.createClient();
 var sub = redis.createClient();
+
+var rooms = [];
+var users = [];
+var messages = [];
  
 io.sockets.on('connection', function (client) {
-    sub.subscribe("chatting");
+    
+    sub.subscribe("chat");
+
     sub.on("message", function (channel, message) {
-        console.log("message received on server from publish ");
-        client.send(message);
+        client.emit("message", message);
     });
-    client.on("message", function (msg) {
-        console.log(msg);
-        if(msg.type == "chat"){
-            pub.publish("chatting",msg.message);
+
+    client.on("join", function (user) {
+        users.push(user);
+        var message = {
+            username : user.username,
+            timestamp : new Date().toString(),
+            content :  'joined the chat'
         }
-        else if(msg.type == "setUsername"){
-            pub.publish("chatting", msg.user + " connected");
-            store.sadd("onlineUsers", msg.user);
-        }
+        pub.publish("chat", JSON.stringify(message));
+    });
+    client.on("message", function (message) {
+        message.timestamp = new Date().toString();
+        messages.push(message);
+        pub.publish("chat", JSON.stringify(message));//gets caught above in sub.on("message")
+    });
+    client.on('reconnecting', function () {
+        console.log("reconnecting");
     });
     client.on('disconnect', function () {
+        console.log("DISCONNECT")
+        pub.publish("chat", client.id + " disconnected");
         sub.quit();
-        pub.publish("chatting","User is disconnected :" + client.id);
     });
      
   });
